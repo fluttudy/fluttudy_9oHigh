f1972b356e1442370233c82d67a9c466aec13270
9oHigh <wkdska957@naver.com>
2024-09-27 15:19:18 +0900

    feat: configure google map

---
+++ flutter_exercise/lib/state_management/riverpod/map_app/screens/map_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MapScreen extends ConsumerWidget {
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    throw UnimplementedError();
+  }
+}
